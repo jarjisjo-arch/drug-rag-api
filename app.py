9c6faf79eb31786d1b0842fb05c3f562c2dc0590
jarjisjo-arch <jarjisjo@gmail.com>
2026-03-08 01:29:05 +0300

    Update app.py

--- app.py
+++ app.py
@@ -44,5 +44,4 @@ def home():
     return "✅ API شغال! أرسل POST request إلى /ask"
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
